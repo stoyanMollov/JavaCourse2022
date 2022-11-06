11d24cdcbe7d9f83949511f1d923be7f273783fb
Stoyan Mollov <mollov9@gmail.com>
2022-11-06 08:25:00 +0200

    StudentAcademy_06

--- src/ExerciseAssociativeArrays/StudentAcademy_06.java
+++ src/ExerciseAssociativeArrays/StudentAcademy_06.java
@@ -1,10 +1,7 @@
 package ExerciseAssociativeArrays;
 
 import java.awt.image.ImageProducer;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class StudentAcademy_06 {
     public static void main(String[] args) {
@@ -16,9 +13,43 @@ public class StudentAcademy_06 {
 
         for (int student = 1; student <= countStudents; student++) {
             String name = scanner.nextLine(); // името на съответния студент.
-            double grade = Integer.parseInt(scanner.nextLine()); // оценката на студента
+            double grade = Double.parseDouble(scanner.nextLine()); // оценката на студента
             // 1. да не е записан
+            if(!studentGrade.containsKey(name)){
+                studentGrade.put(name, new ArrayList<>());
+            }
+
             // 2. да е записан
+
+            studentGrade.get(name).add(grade);
         }
+        // име на студента -> списък с оценки (studentsGrade)
+        // записи: име на студента(key) -> ср. оценка (value)
+        Map<String,Double> averageGradeStudent = new LinkedHashMap<>();
+
+        for (Map.Entry<String,List<Double>> entry:studentGrade.entrySet()) {
+            String studentName = entry.getKey(); // името на студента
+            List<Double> listGrades = entry.getValue(); // списъка с оценките за всеки студент.
+            double averageGrade = getAverageGrade(listGrades);
+            if (averageGrade >= 4.50){
+                averageGradeStudent.put(studentName, averageGrade);
+            }
+        }
+        // отпечатваме: averageGradeStudent
+        averageGradeStudent.entrySet().forEach(entry -> System.out.printf("%s -> %.2f%n",entry.getKey(),entry.getValue()));
+
+
+    }
+
+    private static double getAverageGrade(List<Double> listGrades) {
+        // {4.60, 3.50, 5.98, 4.70, 2.50}
+        // средно аритметично = сбора от оценките / брой
+
+        double sumGrades = 0;
+
+        for (double grade:listGrades) {
+            sumGrades += grade; // сбора от оценките
+        }
+        return sumGrades / listGrades.size();
     }
 }
